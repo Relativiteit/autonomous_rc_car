c0f1693779f6230f57a3e0b153a07380e2c120a9
Goat <Chevre@Goats-MacBook-Pro.local>
2019-11-20 22:42:31 +0100

    Added extra Thresh hold filters

--- README.md
+++ README.md
@@ -4,4 +4,11 @@ This is a project relating to my BSc. thesis.
 
 ## Project Structure
 
+
 `image_threshold.py` is the first python file in this project.
+
+This file contains different filters that can be used within the library of cv2.
+
+* Grayscaled
+* Gausian
+* Adaptive
--- image_threshold.py
+++ image_threshold.py
@@ -6,7 +6,15 @@ cv2.imshow('original img', img)
 
 # Finding the threshold array to be shown in the next image.
 retval, threshold = cv2.threshold(img, 12, 255, cv2.THRESH_BINARY)
+
+grayscaled = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+retval2, threshold2 = cv2.threshold(grayscaled, 12, 255, cv2.THRESH_BINARY)
+
+gaus = cv2.adaptiveThreshold(grayscaled, 255 ,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
+
 cv2.imshow('threshold', threshold)
+cv2.imshow('threshold2', threshold2)
+cv2.imshow('gaus', gaus)
 
 # Wait so the image doesn't instantly disappear.
 cv2.waitKey(0)
